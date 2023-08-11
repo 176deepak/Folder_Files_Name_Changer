e6f73a5f62f33811b6a8dc0a4b13b7d99af301a8
Deepak Saini <deepaksainiofficial17@gmail.com>
2023-08-11 13:52:53 +0530

    Update main.py

--- NameChanger/main.py
+++ NameChanger/main.py
@@ -17,28 +17,3 @@ def name_changer(name, direc_name):
         dst_file = os.path.join(direc_name, filename)
    
         os.rename(src_file, dst_file)
-
-def directory_files_comparer():
-    direc_name1 = Path(str(input("Enter directory1: ")))
-    direc_name2 = Path(str(input("Enter directory2: ")))
-
-    dir1_files = os.listdir(direc_name1)
-    dir2_files = os.listdir(direc_name2)
-    dir1_filenames = []
-    dir2_filenames = []
-
-    for i in dir1_files:
-        name, _ = i.split(".")
-        dir1_filenames.append(name)
-
-    for i in dir2_files:
-        name, _ = i.split(".")
-        dir2_filenames.append(name)
-
-    absent_files = []
-
-    for i in dir2_filenames:
-        if i not in dir1_filenames:
-            absent_files.append(i)
-    
-    return absent_files
